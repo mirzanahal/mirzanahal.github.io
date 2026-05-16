6f0036598399bcca1ee8df8a228f8d4bfa488ec3
mirzanahal <34792714+mirzanahal@users.noreply.github.com>
2026-05-16 13:00:18 +0000

    Deploying to gh-pages from @ mirzanahal/mirzanahal.github.io@19d22a4ca6dc8e0e05d35bd3178d3d6ec5558372 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-our-paper-weakly-supervised-drug-efficiency-estimation-with-confidence-score-application-to-covid-19-drug-discovery-has-accepted-to-miccai-2023",
           title: 'Our paper, Weakly-Supervised Drug Efficiency Estimation with Confidence Score: Application to COVID-19 Drug...',
+          description: "",
+          section: "News",},{id: "news-i-am-attending-miccai-2023-in-vancouver-with-support-from-the-rise-miccai-travel-award-to-present-our-accepted-work-weakly-supervised-drug-efficiency-estimation-with-confidence-score-i-attended-miccai-2023-in-vancouver-canada-supported-by-the-rise-miccai-travel-award-usd-3-000-to-present-our-paper-weakly-supervised-drug-efficiency-estimation-with-confidence-score-application-to-covid-19-drug-discovery-miccai-2023-stanley-park-tour",
+          title: 'I am attending MICCAI 2023 in Vancouver with support from the RISE-MICCAI Travel...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
