c0b9c786604f9c219c65fb17cf658620a2e06a6e
mirzanahal <34792714+mirzanahal@users.noreply.github.com>
2026-05-17 18:16:30 +0000

    Deploying to gh-pages from @ mirzanahal/mirzanahal.github.io@60404b88e5c1754a3f08e16496d5a83b8117573f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -467,11 +467,11 @@ ninja.data = [{
           window.open("mailto:%6D%69%72%7A%61%6E%61%68%61%6C@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/535970159", "_blank");
+          window.open("https://www.linkedin.com/in/nahal-mirzaie", "_blank");
         },
       },{
         id: 'social-scholar',
