df92e08fcd2548991606c1a1f0fb0421f068f627
mirzanahal <34792714+mirzanahal@users.noreply.github.com>
2026-05-17 14:32:35 +0000

    Deploying to gh-pages from @ mirzanahal/mirzanahal.github.io@e3fdc73a0238eaf4f9c1385b64e0a951d1fded90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,11 +437,6 @@ ninja.data = [{
           description: "Biologically-inspired attention masks for gigapixel pathology.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
