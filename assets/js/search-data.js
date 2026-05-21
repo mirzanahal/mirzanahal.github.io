0a73e16971a838efe221d9da135d17a0850d163c
mirzanahal <34792714+mirzanahal@users.noreply.github.com>
2026-05-21 07:28:20 +0000

    Deploying to gh-pages from @ mirzanahal/mirzanahal.github.io@b818e9488c9a94825a3be834eea12c4b14f70744 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,6 +442,11 @@ ninja.data = [{
           description: "Extending GNN expressivity beyond the classical 1-WL limit using Asynchronous Message Passing.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-decidualization-amp-emt-similarities",
+          title: 'Decidualization &amp;amp; EMT Similarities',
+          description: "Investigating shared genomic pathways between pregnancy-induced immune tolerance and cancer metastasis.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-precision-genomics-at-pardisgene",
           title: 'Precision Genomics at PardisGene',
           description: "Automated pipelines for precision oncology and rare disease diagnostics.",
